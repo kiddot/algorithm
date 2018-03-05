dceab6e27435462825987e3e30ee2b925a53c043
kiddot <kiddo1024@163.com>
2018-03-05 21:26:13 +0800

    八大排序

--- src/Sort.java
+++ src/Sort.java
@@ -30,7 +30,7 @@ public class Sort {
 
 
     /**
-     * 希尔排序（Wiki官方版）
+     * 希尔排序
      *
      * 1. 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；（注意此算法的gap取值）
      * 2. 按增量序列个数k，对序列进行k 趟排序；
@@ -61,5 +61,74 @@ public class Sort {
         int temp = target[i];
         target[i] = target[j];
         target[j] = temp;
+    }
+
+
+
+    /**
+     * 选择排序
+     *
+     * 1. 从待排序序列中，找到关键字最小的元素；
+     * 2. 如果最小元素不是待排序序列的第一个元素，将其和第一个元素互换；
+     * 3. 从余下的 N - 1 个元素中，找出关键字最小的元素，重复①、②步，直到排序结束。
+     *    仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+     * @param arr  待排序数组
+     */
+    public static void selectionSort(int[] arr){
+        for(int i = 0; i < arr.length-1; i++){
+            int min = i;
+            for(int j = i+1; j < arr.length; j++){    //选出之后待排序中值最小的位置
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            if(min != i){
+                int temp = arr[min];      //交换操作
+                arr[min] = arr[i];
+                arr[i] = temp;
+                System.out.println("Sorting:  " + Arrays.toString(arr));
+            }
+        }
+    }
+
+
+
+    /**
+     * 堆排序
+     *
+     * 1. 先将初始序列K[1..n]建成一个大顶堆, 那么此时第一个元素K1最大, 此堆为初始的无序区.
+     * 2. 再将关键字最大的记录K1 (即堆顶, 第一个元素)和无序区的最后一个记录 Kn 交换, 由此得到新的无序区K[1..n−1]和有序区K[n], 且满足K[1..n−1].keys⩽K[n].key
+     * 3. 交换K1 和 Kn 后, 堆顶可能违反堆性质, 因此需将K[1..n−1]调整为堆. 然后重复步骤②, 直到无序区只有一个元素时停止.
+     * @param arr  待排序数组
+     */
+    public static void heapSort(int[] arr){
+        for(int i = arr.length; i > 0; i--){
+            max_heapify(arr, i);
+
+            int temp = arr[0];      //堆顶元素(第一个元素)与Kn交换
+            arr[0] = arr[i-1];
+            arr[i-1] = temp;
+        }
+    }
+
+    private static void max_heapify(int[] arr, int limit){
+        if(arr.length <= 0 || arr.length < limit) return;
+        int parentIdx = limit / 2;
+
+        for(; parentIdx >= 0; parentIdx--){
+            if(parentIdx * 2 >= limit){
+                continue;
+            }
+            int left = parentIdx * 2;       //左子节点位置
+            int right = (left + 1) >= limit ? left : (left + 1);    //右子节点位置，如果没有右节点，默认为左节点位置
+
+            int maxChildId = arr[left] >= arr[right] ? left : right;
+            if(arr[maxChildId] > arr[parentIdx]){   //交换父节点与左右子节点中的最大值
+                int temp = arr[parentIdx];
+                arr[parentIdx] = arr[maxChildId];
+                arr[maxChildId] = temp;
+            }
+        }
+        System.out.println("Max_Heapify: " + Arrays.toString(arr));
     }
 }
